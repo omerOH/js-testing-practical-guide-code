85da161305218cca563eb5e516ada3275aa5b2bf
omer haziza <omerchaziza@gmail.com>
2025-06-09 19:55:39 +0300

    adding some hooks and concurrent for parallel tests
    the Hooks: beforeAll beforeEach afterEachafterAll

--- lesson 49 Advanced Testing Concepts/Why Hooks/hooks/hooks.test.js
+++ lesson 49 Advanced Testing Concepts/Why Hooks/hooks/hooks.test.js
@@ -1,47 +1,69 @@
-import { it, expect } from 'vitest';
+import {
+  it,
+  describe,
+  expect,
+  beforeAll,
+  beforeEach,
+  afterEach,
+  afterAll,
+} from "vitest";
 
-import { User } from './hooks';
+import { User } from "./hooks";
 
-it('should update the email', () => {
-  const testEmail = 'test@test.com';
-  const newTestEmail = 'test2@test.com';
+const testEmail = "test@test.com";
+// let user = new User(testEmail);
+let user;
 
-  const user = new User(testEmail);
-  user.updateEmail(newTestEmail);
+beforeAll(() => {
+  // will run at the begining of all of the tests (can be inside of describe for groupint tests)
+  user = new User(testEmail);
+  console.log("beforeAll()");
+});
+beforeEach(() => {
+  // will run at the begining of every test (can be inside of describe for groupint tests)
+  user = new User(testEmail);
+  console.log("beforeEach()");
+});
+afterEach(() => {
+  // will run at the ending of every test (can be inside of describe for groupint tests)
+  // user = new User(testEmail);
+  console.log("afterEach()");
+});
+afterAll(() => {
+  // will run at the ending of all of the tests (can be inside of describe for groupint tests)
+  console.log("afterAll()");
+});
+
+it("should update the email", () => {
+  const newTestEmail = "test2@test.com";
+
+  user.updateEmail(newTestEmail); // this line will manipulate the user object for the rest of the tests witout the hooks
 
   expect(user.email).toBe(newTestEmail);
 });
 
-it('should have an email property', () => {
-  const testEmail = 'test@test.com';
+// describe.concurrent(); // this will run all the test inside of it in parallel
 
-  const user = new User(testEmail);
-
-  expect(user).toHaveProperty('email');
+it.concurrent("should have an email property", () => {
+  // this will run the test in parallel with other tests and will not wait for the other one to be finished. it's good for tests that doesn't depend  on the previous test
+  expect(user).toHaveProperty("email");
 });
 
-it('should store the provided email value', () => {
-  const testEmail = 'test@test.com';
-
-  const user = new User(testEmail);
-
+it.concurrent("should store the provided email value", () => {
   expect(user.email).toBe(testEmail);
 });
 
-it('should clear the email', () => {
-  const testEmail = 'test@test.com';
-
-  const user = new User(testEmail);
+it.concurrent("should clear the email", () => {
   user.clearEmail();
 
-  expect(user.email).toBe('');
+  expect(user.email).toBe("");
 });
 
-it('should still have an email property after clearing the email', () => {
-  const testEmail = 'test@test.com';
+it.concurrent(
+  "should still have an email property after clearing the email",
+  () => {
+    user.clearEmail();
 
-  const user = new User(testEmail);
-  user.clearEmail();
-
-  expect(user).toHaveProperty('email');
-});
+    expect(user).toHaveProperty("email");
+  }
+);
